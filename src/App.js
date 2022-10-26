ad36fb4f50b6f53ea08438af0f99fb5db99ade6a
nandagopan <nandagopankalpetta365@gmail.com>
2022-10-26 18:05:08 +0530

    2nd commit: without css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,19 @@ import React from 'react'
 export const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline text-orange-800">Hello World</h1>
+      <div>
+        <div>
+          <input type="text" placeholder='Enter Place...' />
+        </div>
+        <div>
+          <h2>Chennai</h2>
+          <h3>5.2 C</h3>
+          <div>
+            <p>min : 5.2 Cel |</p>
+            <p>max : 5.2 Cel </p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
